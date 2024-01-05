f230c79b87622813a0e7249241707ac78c108464
thanhtn <thanhtn@mirabo-global.com>
2024-01-05 10:50:37 +0700

    feat: add pipeline

---
+++ greetings/greeting.go
@@ -0,0 +1,13 @@
+// greetings.go
+package greetings
+
+import "fmt"
+
+// Hello returns a greeting message containing the provided name.
+func Hello(name string) (string, error) {
+    if name == "" {
+        return "", fmt.Errorf("empty name")
+    }
+    message := fmt.Sprintf("Hello, %s!", name)
+    return message, nil
+}
