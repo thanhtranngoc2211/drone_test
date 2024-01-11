35143c00582401b8c506eea6107f8bdf4ca7ea19
thanhtn <thanhtn@mirabo-global.com>
2024-01-11 09:52:01 +0700

    chore(drone): test notify

--- google_chat/webhook.py
+++ google_chat/webhook.py
@@ -2,15 +2,15 @@ from json import dumps
 from httplib2 import Http
 import os
 
-build_number = os.getenv('BUILD_NUMBER')
-commit = os.getenv('COMMIT')
-project = os.getenv('PROJECT')
-author = os.getenv('AUTHOR_EMAIL')
+build_number = os.getenv('DRONE_BUILD_NUMBER')
+commit = os.getenv('DRONE_COMMIT')
+project = os.getenv('DRONE_REPO')
+author = os.getenv('DRONE_COMMIT_AUTHOR_EMAIL')
 
 def main():
     """Google Chat incoming webhook quickstart."""
     url = "https://chat.googleapis.com/v1/spaces/AAAA96rxQ2I/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=hvSeDNTsFONz40n89_e-R8SnGVptn8oWVULNujvcB5M"
-    app_message = {"text": f'User {author} triggered pipeline {build_number} - Commit: "{commit}" - Project: "{project}" completed!'}
+    app_message = {"text": f'Pipeline {build_number} - Commit: "{commit}" - Project: "{project}" completed!\nTriggered by {author}'}
     message_headers = {"Content-Type": "application/json; charset=UTF-8"}
     http_obj = Http()
     response = http_obj.request(
